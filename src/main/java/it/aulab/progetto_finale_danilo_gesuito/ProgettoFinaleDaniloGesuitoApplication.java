e5187d9c861cf59a5d550bbbce5888416f6dea54
Danilo Gesuito <danilo.gesuito@hotmail.it>
2025-03-31 20:03:15 +0200

    Inizio User Story 1

---
+++ src/main/java/it/aulab/progetto_finale_danilo_gesuito/ProgettoFinaleDaniloGesuitoApplication.java
@@ -0,0 +1,13 @@
+package it.aulab.progetto_finale_danilo_gesuito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoFinaleDaniloGesuitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoFinaleDaniloGesuitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/aulab/progetto_finale_danilo_gesuito/ProgettoFinaleDaniloGesuitoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.aulab.progetto_finale_danilo_gesuito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoFinaleDaniloGesuitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
